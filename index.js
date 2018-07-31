b0b2a6cd28f9e812d22c4808fccf6cca37acb25f
root <andrew.n.sadowski@gmail.com>
2018-07-30 17:44:59 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+const parser = require("subtitles-parser");
+
+//Parse and save srt
+const srt = fs.readFileSync("test.srt", "utf8");
+var data = parser.fromSrt(srt);
+
+const checkTimeStamps = sub => {
+  for (let i = 0; i < sub.length; i++) {
+    console.log("outer loop", sub[i]);
+
+    for (let j = i + 1; j <= i + 1; j++) {
+      let iMS = sub[i].endTime.substr(9, 3);
+      let jMS = sub[j].startTime.substr(9, 3);
+
+      console.log("inner: ", iMS);
+      if (sub[i].endTime > sub[j].startTime) {
+        console.log("ITS BIGGER", sub[i].endTime);
+      }
+    }
+  }
+};
+
+checkTimeStamps(data);
