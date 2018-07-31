e48c31decdcc2413effef82291d91f08ec5f1753
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-30 19:53:18 -0700

    added start and start:watch scripts to package.json as well as Nodemon as a dev-dependency

--- index.js
+++ index.js
@@ -5,26 +5,23 @@ const parser = require('subtitles-parser');
 const srt = fs.readFileSync('test.srt', 'utf8');
 var data = parser.fromSrt(srt);
 
-const updateTimeStamps = sub => {
+const updateMSTimeStamps = sub => {
   for (let i = 0; i < sub.length; i++) {
-    console.log('outer loop');
-
     for (let j = i + 1; j <= i + 1; j++) {
       if (sub[j] === undefined) return;
 
+      //save substring of MS for both preceeding start and end times
       let iMS = sub[i].endTime.substr(9, 3);
       let jMS = sub[j].startTime.substr(9, 3);
 
-      console.log('inner');
       if (iMS > jMS) {
         console.log(`iMS: ${iMS} should be the same as jMS: ${jMS}`);
         sub[j].startTime = sub[j].startTime.replace(/\d{3}/g, iMS);
-        console.log(sub[j], sub[i]);
       }
     }
 
     const updatedSrt = parser.toSrt(sub);
-    fs.writeFile('updatedTest.srt', updatedSrt, err => {
+    fs.writeFileSync('updatedTest.srt', updatedSrt, err => {
       if (err) throw err;
       console.log('Updated Srt created');
     });
@@ -32,7 +29,7 @@ const updateTimeStamps = sub => {
   return sub;
 };
 
-updateTimeStamps(data);
+updateMSTimeStamps(data);
 
 // const updatedSub = updateTimeStamps(data);
 // const updatedSrt = parser.toSrt(updatedSub);
