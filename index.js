ac02f3c66daca4eb99e0659b04c0b26b329ef8eb
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-31 19:52:01 -0700

    added usage and example strings to yargs output

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const parser = require("subtitles-parser");
-var fs = require("fs");
-const argv = require("yargs")
-  .alias("f", "filePath")
-  .help("h").argv;
+const parser = require('subtitles-parser');
+var fs = require('fs');
+const argv = require('yargs')
+  .alias('f', 'filePath')
+  .usage('Usage: add a file with the -f flag')
+  .example('node index.js -f "fileName.ext"')
+  .help('h').argv;
 
 let filePath;
 
-if (argv === "f") {
+if (argv === 'f') {
   filePath = argv.f;
 } else {
-  filePath = "test.srt";
+  filePath = 'test.srt';
 }
 
 //Parse and save srt
-const srt = fs.readFileSync(filePath, "utf8");
+const srt = fs.readFileSync(filePath, 'utf8');
 var data = parser.fromSrt(srt);
 
 const updateMSTimeStamps = sub => {
@@ -26,20 +28,15 @@ const updateMSTimeStamps = sub => {
       let jMS = sub[j].startTime.substr(9, 3);
 
       if (iMS > jMS) {
-        console.log(
-          `iMS: ${iMS} should be the same as jMS: ${jMS}`
-        );
-        sub[j].startTime = sub[j].startTime.replace(
-          /\d{3}/g,
-          iMS
-        );
+        console.log(`iMS: ${iMS} should be the same as jMS: ${jMS}`);
+        sub[j].startTime = sub[j].startTime.replace(/\d{3}/g, iMS);
       }
     }
 
     const updatedSrt = parser.toSrt(sub);
-    fs.writeFileSync("updatedTest.srt", updatedSrt, err => {
+    fs.writeFileSync('updatedTest.srt', updatedSrt, err => {
       if (err) throw err;
-      console.log("Updated Srt created");
+      console.log('Updated Srt created');
     });
   }
   return sub;
