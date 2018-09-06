43e1547666498b6d8b6d416cc99685c53dd77440
admin <andrew.n.sadowski@gmail.com>
2018-09-06 14:24:39 -0700

    added another test block to test file

--- msNormalizer.test.js
+++ msNormalizer.test.js
@@ -2,7 +2,9 @@ const mocha = require('mocha');
 const expect = require('expect');
 const parser = require('subtitles-parser');
 const fs = require('fs');
-const { msNormalizer } = require('./msNormalizer.js');
+const {
+  msNormalizer
+} = require('./msNormalizer.js');
 
 const data = fs.readFileSync('test.srt', 'utf8');
 const parsedData = parser.fromSrt(data);
@@ -11,4 +13,9 @@ describe('msNormalizer Tests', done => {
   it('should return an object of matching length to the input', () => {
     expect(Object.keys(parsedData).length).toBe(5);
   });
+
+  it("should be of type object", () => {
+    expect(typeof parsedData).toBe("object");
+  });
+
 });
