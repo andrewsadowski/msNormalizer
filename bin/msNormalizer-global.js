f25705a67ef2e1f06142e695f55de09ad047264d
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-30 14:35:08 -0700

    added utilities.js file with path parsing helper functions

--- bin/msNormalizer-global.js
+++ bin/msNormalizer-global.js
@@ -4,5 +4,9 @@ const msNormalizer = require('../msNormalizer.js');
 const argv = require('yargs').argv;
 
 console.log(
-  msNormalizer(typeof argv.file === 'string' ? argv.file : undefined)
+  msNormalizer(
+    typeof argv.file || typeof argv.f === 'string'
+      ? argv.file || argv.f
+      : undefined
+  )
 );
