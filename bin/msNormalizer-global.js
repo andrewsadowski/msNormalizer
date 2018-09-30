a11c912181757003667996d3c4a51b31cb949443
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-30 16:12:26 -0700

    moved fs writeFile function to utils

--- bin/msNormalizer-global.js
+++ bin/msNormalizer-global.js
@@ -2,11 +2,8 @@
 
 const msNormalizer = require('../msNormalizer.js');
 const argv = require('yargs').argv;
-
-console.log(
-  msNormalizer(
-    typeof argv.file || typeof argv.f === 'string'
-      ? argv.file || argv.f
-      : undefined
-  )
+msNormalizer(
+  typeof argv.file || typeof argv.f === 'string'
+    ? argv.file || argv.f
+    : undefined
 );
