1e532d0b71662891fd6271e207dab4b63797ec30
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-10-02 21:38:15 -0700

    updated global file to include argv check

--- bin/msNormalizer-global.js
+++ bin/msNormalizer-global.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 
 const msNormalizer = require('../msNormalizer.js');
-const argv = require('yargs').argv;
+const argv = require("yargs")
+  .alias("f", "filePath")
+  .usage("Usage: add a file with the -f flag")
+  .example('node index.js -f "fileName.ext"')
+  .help("h").argv;
+
+let filePath;
+
+if (argv.f) {
+  filePath = argv.f;
+} else {
+  filePath = "test.srt";
+}
+
 msNormalizer(
   typeof argv.file || typeof argv.f === 'string'
     ? argv.file || argv.f
