1f5193c91dc0cbbc07b90970342e7c76ef962be4
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-10-16 19:21:19 -0700

    updated msNormalizer-global.js to work with current -d flags

--- bin/msNormalizer-global.js
+++ bin/msNormalizer-global.js
@@ -1,62 +1,39 @@
 #!/usr/bin/env node
 
-const parser = require("subtitles-parser");
-const fs = require("fs");
-const path = require("path");
+const parser = require('subtitles-parser');
+const fs = require('fs');
+const path = require('path');
 const {
   getDefaultDirPath,
   getFileName,
-  writeSubToFile
-} = require("../lib/utils.js");
-const argv = require("yargs")
-  .alias("f", "filePath")
-  .usage("Usage: add a file path with the -f flag")
+  writeSubToFile,
+  msNormalizer,
+  handleDirOfSubs
+} = require('../lib/utils.js');
+const argv = require('yargs')
+  .alias('f', 'filePath')
+  .usage('Usage: add a file path with the -f flag')
   .example('msNormalizer -f "/absolute/path/to/file.srt"')
-  .help("h").argv;
+  .alias('d', 'dirPath')
+  .usage('Usage: add a directory path with the -d flag')
+  .example('msNormalizer -d "/absolute/path/of/dir/"')
+  .help('h').argv;
 
 let filePath;
+let dirPath;
 
 if (argv.f) {
   filePath = argv.f;
+} else if (argv.d) {
+  dirPath = argv.d;
 } else {
-  filePath = "test.srt";
+  filePath = 'test.srt';
 }
 
-/**
- * @param {string} filePath - accepts a string with a path to the subtitle
- * @return {string} sub - returns a subtitle file as well as a properly formatted srt
- */
-const msNormalizer = (async filePath => {
-  const srt = fs.readFileSync(filePath, "utf8");
-
-  const parsedDirPath = getDefaultDirPath(filePath);
-  const subtitleFileName = getFileName(filePath);
-
-  const outputNameAndPath = path.join(
-    parsedDirPath + subtitleFileName + "_msUpdated.srt"
-  );
-
-  let sub = parser.fromSrt(srt);
-
-  for (let i = 0; i < sub.length; i++) {
-    for (let j = i + 1; j <= i + 1; j++) {
-      if (sub[j] === undefined) return;
-      const msCheckAndUpdate = await (() => {
-        //save substring of MS for both preceeding start and end times
-        let iMS = sub[i].endTime.substr(9, 3);
-        let iSS = sub[i].endTime.substr(6, 2);
-        let jMS = sub[j].startTime.substr(9, 3);
-        let jSS = sub[j].startTime.substr(6, 2);
-
-        if (iMS > jMS && iSS === jSS) {
-          console.log(`iMS: ${iMS} should be the same as jMS: ${jMS}`);
-          sub[j].startTime = sub[j].startTime.replace(/\d{3}/g, iMS);
-        }
-      })();
-    }
-    const updatedSrt = await parser.toSrt(sub);
-    await writeSubToFile(outputNameAndPath, updatedSrt);
+const executeRequest = (path => {
+  if (filePath) {
+    msNormalizer(path);
+  } else if (dirPath) {
+    handleDirOfSubs(path);
   }
-
-  return sub;
-})(filePath);
+})(filePath || dirPath);
